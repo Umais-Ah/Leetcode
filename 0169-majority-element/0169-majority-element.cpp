40fe969773e7eba8667f1a3bdbad2a5f00f96406
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-07-27 04:48:45 +0500

    Time: 0 ms (100%), Space: 28.1 MB (86.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,11 +4,13 @@ public:
      int el=arr[0]; int cnt=0; int n=arr.size();
      
     for(int i=0;i<n;i++){
-        if(arr[i]==el) cnt++;
-        else cnt--;
         if(cnt==0){
-            el=arr[i+1];
+            cnt=1;
+            el=arr[i];
         }
+       else if(arr[i]==el) cnt++;
+       else cnt--;
+
     }
 
     return el;

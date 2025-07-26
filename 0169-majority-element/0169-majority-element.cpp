9a0478612bb890a359232a398c2c4dcb28b89e9b
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-07-27 03:22:07 +0500

    Time: 3 ms (29.3%), Space: 28.3 MB (27.09%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+     int n=arr.size();
+     map<int,int> m;  
+     for(int i=0;i<n;i++){
+         m[arr[i]]++;
+     }
+     
+     for(int i=0;i<n;i++){
+        if(m[arr[i]] > n/2){
+           return arr[i];
+        }
+     }
+     return {};
+    }
+};

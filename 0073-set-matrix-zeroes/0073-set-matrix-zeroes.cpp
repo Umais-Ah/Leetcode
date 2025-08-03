96046b98e99a953e55f7b8dc9e42b2168836d628
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-03 14:55:01 +0500

    Time: 3 ms (10.68%), Space: 18.7 MB (51.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int m=matrix.size();
+        int n=matrix[0].size();
          int col0 = 1;
-    // step 1: Traverse the matrix and
-    // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+  
+               
+        // Step 1: Mark
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
             if (matrix[i][j] == 0) {
-                // mark i-th row:
                 matrix[i][0] = 0;
-
-                // mark j-th column:
                 if (j != 0)
                     matrix[0][j] = 0;
                 else
@@ -21,30 +19,32 @@ public:
         }
     }
 
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                // check for col & row:
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
+    // Step 2: Zero rest (excluding 1st row and 1st col)
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
             }
         }
     }
 
-    //step 3: Finally mark the 1st col & then 1st row:
+    // Step 3: Handle 1st row
     if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < n; j++) {
             matrix[0][j] = 0;
         }
     }
+
+    // Step 4: Handle 1st column
     if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             matrix[i][0] = 0;
         }
     }
 
-
+ 
     }
+
+
+    
 };

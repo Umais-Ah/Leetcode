b13dfd0ba8dc5efcc9b775e8eca0bb412c33c2e0
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-04 17:49:03 +0500

    Time: 36 ms (85.76%), Space: 45.5 MB (43.59%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
-    int count=0;
     int n=arr.size();
+    int count=0; int presum=0;
+    unordered_map<int,int> hashmap;
+    hashmap[0]=1;
     for(int i=0;i<n;i++){
-          int sum=0;
-     for(int j=i;j<n;j++){
-           sum+=arr[j];
-           if(sum==k){
-            count++;
-           }
+      presum+=arr[i];
+      int prefix=presum-k;
+      //if exist
+      if(hashmap.find(prefix)!=hashmap.end()){
+        count+=hashmap[prefix];
+        hashmap[presum]++;
+      }
+      else{
+        hashmap[presum]++;
+      }
     }
-   }
 
-   return count;
+    return count;
     }
 };

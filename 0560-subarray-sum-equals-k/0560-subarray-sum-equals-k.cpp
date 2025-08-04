796e208be991513a970dc913c7521837027891b3
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-04 15:25:40 +0500

    Time: 2480 ms (14.76%), Space: 34.3 MB (93.63%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+    int count=0;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+          int sum=0;
+     for(int j=i;j<n;j++){
+           sum+=arr[j];
+           if(sum==k){
+            count++;
+           }
+    }
+   }
+
+   return count;
+    }
+};

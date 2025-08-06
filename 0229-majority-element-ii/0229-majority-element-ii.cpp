fb7397f33894651a7c02904c93123883a1a6541a
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-06 18:58:07 +0500

    Time: 0 ms (100%), Space: 23.4 MB (99.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,37 @@
 class Solution {
 public:
 vector<int> majorityElement(vector<int>& arr) {
-   vector<int> v;   int n=arr.size(); 
-   unordered_map<int,int> hashmap;
+    vector<int> v; int cnt1=0; int cnt2=0;  int n=arr.size();
+    int el1=INT_MIN;
+    int el2=INT_MIN;
    for(int i=0;i<n;i++){
-     hashmap[arr[i]]++;
-     if(hashmap[arr[i]]==n/3+1){
-        v.push_back(arr[i]);
+     if(cnt1==0 && arr[i] != el2){
+        cnt1=1;
+        el1=arr[i];
      }
-   }
-  return v;
+     else if(cnt2==0 && arr[i] != el1){
+        cnt2=1;
+        el2=arr[i];
+     }
+     else if(arr[i]==el1) cnt1++;
+     else if(arr[i]==el2) cnt2++;
+     else{
+        cnt1--; cnt2--;
+     }
+    }
+
+    cnt1=0;  cnt2=0;
+
+     for(int i=0;i<n;i++){
+        if(arr[i]==el1) cnt1++;
+        if(arr[i]==el2) cnt2++;
+     }
+
+     int  minimum=n/3+1;
+     if(cnt1 >= minimum)  v.push_back(el1);
+     if(cnt2 >= minimum)  v.push_back(el2);
+
+     return v;
 }
 
 };

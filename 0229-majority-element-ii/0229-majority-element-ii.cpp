c65f08af07ac3825da041a043dc53501050c3b10
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-06 16:08:12 +0500

    Time: 6 ms (44.81%), Space: 26.5 MB (18.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& arr) {
-    vector<int> v; int n=arr.size();
-    unordered_map<int,int> hashmap;
-    for(int i=0;i<n;i++){
-      hashmap[arr[i]]++;
-     if(hashmap[arr[i]] > n/3 && (v.size()==0 || v[0] != arr[i])){
+vector<int> majorityElement(vector<int>& arr) {
+   vector<int> v;   int n=arr.size(); int minimum=n/3+1;
+   unordered_map<int,int> hashmap;
+   for(int i=0;i<n;i++){
+     hashmap[arr[i]]++;
+     if(hashmap[arr[i]]==minimum){
         v.push_back(arr[i]);
-        hashmap[arr[i]]=-1; 
-      }
-      if(v.size()==2){
-        break;
-      }
-    }
- 
-     return v;
-    }
+     }
+   }
+  return v;
+}
 
 };

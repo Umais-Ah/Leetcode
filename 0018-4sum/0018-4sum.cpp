c954b45b74f5bdb325f200d98f1ee85894006ba0
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-07 18:24:41 +0500

    Time: 24 ms (39%), Space: 17.5 MB (57.38%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
 vector<vector<int>> fourSum(vector<int>& arr, int target) {
- set<vector<int>> s;  int n=arr.size();
-  
-   for(int i=0;i<n;i++){
-       for(int j=i+1;j<n;j++){
-            set<long long> hashset;
-           for(int k=j+1;k<n;k++){
-               long long l=target-arr[i];
-               l-=arr[j];
-               l-=arr[k];
-                     if(hashset.find(l)!=hashset.end()){
-                        vector<int> temp={arr[i] , arr[j] , arr[k] , int(l)};
-                        sort(temp.begin(),temp.end());
-                        s.insert(temp);
-                     }
-                     hashset.insert(arr[k]);
-               }
-               
-           }
-       }
-vector<vector<int>> ans(s.begin(),s.end());         
-return ans;
+vector<vector<int>> ans; int n=arr.size();
+
+ sort(arr.begin(),arr.end());
+ for(int i=0;i<n;i++){
+    if(i > 0 && arr[i] ==arr[i-1]) continue;
+    for(int j=i+1;j<n;j++){
+        if(j > i+1 && arr[j] ==arr[j-1]) continue;
+      int k=j+1;
+      int l=n-1;
+      while (k < l){
+         long long sum = arr[i] + arr[j];
+        sum+=arr[k]; 
+        sum+=arr[l];
+      if(sum > target) l--;
+      else if(sum < target) k++;
+      else{
+         vector<int> temp={arr[i],arr[j],arr[k],arr[l]};
+         ans.push_back(temp);
+         k++;
+         l--;
+         while(arr[k]==arr[k-1] && k<l) k++;
+         while(arr[l]==arr[l+1] && k<l) l--;
+      }
+      }
+    }
+ }
+         
+ return ans;
     }
 };

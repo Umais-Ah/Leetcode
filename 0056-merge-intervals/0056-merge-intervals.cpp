d17e5b438f67deca0c7b871a3c847e92404d4916
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-09 17:24:07 +0500

    Time: 374 ms (5.08%), Space: 24.6 MB (19.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 vector<vector<int>> merge(vector<vector<int>>& arr) {
-  int n=arr.size();  vector<int> hash(n,0);    set<vector<int>> st;
+  int n=arr.size();  vector<int> hash(n,0); vector<vector<int>> ans;    
   
    sort(arr.begin(),arr.end());
 
@@ -9,16 +9,14 @@ vector<vector<int>> merge(vector<vector<int>>& arr) {
    if(hash[i]==1) continue;
     for(int j=0;j<n;j++){
           if(arr[i][1] >= arr[j][0]  && arr[i][0] <= arr[j][1]  ){
-            arr[i][0]=min(arr[i][0],arr[j][0]);
             arr[i][1]=max(arr[i][1],arr[j][1]);
             hash[j]=1;
           }
     }
     vector<int> temp={{arr[i][0], arr[i][1]}};
-    st.insert(temp); 
+    ans.push_back(temp);
    }
 
-   vector<vector<int>> ans={st.begin(),st.end()};
    
    return ans;
     }

13f51a4d89f067606e5e403f96fb435a46f24293
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-09 17:57:10 +0500

    Time: 2 ms (93.32%), Space: 23.9 MB (31.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
 vector<vector<int>> merge(vector<vector<int>>& arr) {
-  int n=arr.size();  vector<int> hash(n,0); vector<vector<int>> ans;    
-  
-   sort(arr.begin(),arr.end());
-
-   for(int i=0;i<n;i++){
-   if(hash[i]==1) continue;
-    for(int j=i+1;j<n;j++){
-          if(arr[i][1] >= arr[j][0]  && arr[i][0] <= arr[j][1]  ){
-            arr[i][1]=max(arr[i][1],arr[j][1]);
-            hash[j]=1;
-          }
-            else if(arr[i][1] < arr[j][0]){
-            break;
-          }
-    }
-    ans.push_back({arr[i][0],arr[i][1]});
-   }
-
+ int n=arr.size(); vector<vector<int>> ans;    
    
-   return ans;
+    sort(arr.begin(),arr.end());
+ 
+    for(int i=0;i<n;i++){
+     int start=arr[i][0];
+     int end=arr[i][1];
+     if(!ans.empty() && end <=ans.back()[1]) continue;    
+        for(int j=i+1;j<n;j++){
+           if(end >= arr[j][0]){
+             end=max(end,arr[j][1]);
+           }
+           else if(end < arr[j][0]){
+             break;
+           }
+     }
+     ans.push_back({start,end});
+    }
+
+    return ans;
 
  
    }

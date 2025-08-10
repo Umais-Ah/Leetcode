f940aea07923aa74da929975c7e6820a3afd593b
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-10 20:00:32 +0500

    Time: 0 ms (100%), Space: 26.1 MB (50.95%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& arr) {
+ vector<int> findErrorNums(vector<int>& arr) {
     int n=arr.size();
-     vector<int> ans;  vector<int> hash(n+1, 0);
-       hash[0]=1;
-      for(int i=0;i<n;i++){
-        hash[arr[i]]++;
-        }
+    vector<int> hash(n+1, 0); int repeating=-1; int missing=-1;
+  
+  for(int i=0;i<n;i++){
+    hash[arr[i]]++;
+  }
 
-    for(int i=0;i<n+1;i++){
-        if(hash[i] == 2){
-               ans.push_back(i);
-        }
+  for(int i=1;i<n+1;i++){
+    
+    if(hash[i]==0){
+        missing=i;
+    }
+    if(hash[i]==2){
+        repeating=i;
     }
 
-
-    for(int i=0;i<n+1;i++){
-        if(hash[i] == 0){
-               ans.push_back(i);
-        }
+    if(missing !=-1 && repeating != -1){
+        break;
+     }
     }
-        
-     
+    
+    vector<int> ans={repeating,missing}; 
+    return ans;
 
-        return ans;
         
     }
     

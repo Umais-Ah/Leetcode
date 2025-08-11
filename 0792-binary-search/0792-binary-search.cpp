0f82e34636b37916fdfc91f94cc03e8dabedf8a2
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-11 23:46:40 +0500

    Time: 0 ms (100%), Space: 31.2 MB (98.36%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+         if(arr[i]==target){
+               return i;            
+         }  
+    }
+    return -1;
+    }
+};

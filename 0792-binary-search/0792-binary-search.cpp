72948bbae0bb92c64360973f8c2653bc57b2beeb
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-12 01:20:50 +0500

    Time: 0 ms (100%), Space: 31.4 MB (45.18%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-         if(arr[i]==target){
-               return i;            
-         }  
-    }
-    return -1;
+  int search(vector<int>& arr, int target) {
+  int n=arr.size();
+  int high=n-1;
+  int low=0;
+ 
+  while(high  >= low){
+  int mid=(high + low)/2;
+  if(arr[mid] > target){
+    high=mid-1;
+  }
+  else if(arr[mid] < target) {
+    low=mid+1;
+  }
+  else{
+    return mid;
+  }
+  }
+  return -1;
     }
 };

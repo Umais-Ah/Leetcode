2a7c5e203da19d77030567898827442d66fe9e76
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-13 20:43:51 +0500

    Time: 0 ms (100%), Space: 15.3 MB (32.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-     map<int,int> m; int n=arr.size();
-    for(int i=0;i<n;i++){
-      m[arr[i]]=i;
-   }
-
-   sort(arr.begin(),arr.end());
-   int ans=-1;
-   int low=0; int high=n-1;
-
-   while(low<=high){
-   int mid=(low+ high)/2;
-   if(arr[mid]==target){
-     ans=mid;
-     break;
-   }
-   else if(arr[mid] > target) high=mid-1;
-   else  low=mid+1;
-   }
-
-  if(ans!=-1){
-    return m[arr[ans]];
-  }
+int search(vector<int>& arr, int target) {
+  int n=arr.size();
+ int low=0; int high=n-1;
+    while(low <= high){
+    int mid=(low + high)/2;
   
-  return ans;
+   if(arr[mid]==target) return mid;
+   else if(arr[mid] <= arr[high]){ 
+       if(target >=arr[mid] && target <= arr[high]) low=mid+1;
+       else high=mid-1; 
+    }
+    else{
+        if(target <=arr[mid] && target >=arr[low]) high=mid-1;
+        else low=mid+1;
+    }
+}
+
+return -1;
     }
 };

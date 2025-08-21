5257630d14065e416be04497db0eaf8b73754721
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-08-21 18:33:16 +0500

    Time: 8 ms (14.72%), Space: 8.5 MB (85.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int mySqrt(int n) {
     int ans=0;
-    for(int i=0;i<=n;i++){
+    for(int i=1;i<=n;i++){
         if((long long)i*i > n ) break;
         else ans=i; 
     }

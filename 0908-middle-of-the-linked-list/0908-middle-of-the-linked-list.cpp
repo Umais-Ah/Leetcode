44f4593c181624923dfd3cb36f9d9252a5bab00d
Umais Ahmed <142818672+Umais-Ah@users.noreply.github.com>
2025-09-07 01:31:42 +0500

    Time: 0 ms (100%), Space: 10.2 MB (2.11%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -11,19 +11,15 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-          ListNode* temp=head; int count=0;
-          while(temp!=NULL){
-            count++;
-            temp=temp->next;
-          }
+      ListNode* slow=head;
+      ListNode* fast=head;
+        
+      while(fast!=NULL && fast->next!=nullptr ){
+        slow=slow->next;
+        fast=fast->next->next;
+      }
 
-       temp=head;
-       
-        for(int i=0;i<count/2;i++){
-            temp=temp->next;
-        }
-
-        return temp;
+     return slow;
 
     }
 };
